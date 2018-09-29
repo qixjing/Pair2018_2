12dba7ea3b73b46195f34ffc3c567fddf37490a2
linzenghan <42675313+linzenghan@users.noreply.github.com>
2018-09-30 06:56:49 +0800

    为测试类代码修改源代码
    1.重写了一个div（）方法，使之能够在源代码和测试类中分别进行，在保证方法里的算法一致的前提下，对两者代码不产生影响
    2.重构了一个无参构造函数，方便测试类代码调用

--- Pair2_211606335_211606318/src/MathExam6335.java
+++ Pair2_211606335_211606318/src/MathExam6335.java
@@ -1,3 +1,5 @@
+package com.Pair2.MathExam;
+
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -39,25 +41,32 @@ public class MathExam6335 {
 	int firstNumber, secondNumber;		
 	int symbol;	//运算符号判断
 	int result;
-	static int grade;
-	static int count;		
+	public static int grade; 
+	public static int count;	
+	public boolean w1 = true;
+	public boolean w2 = true;
 	boolean calflag = true;  //返回一个布尔类型，控制题目规范
 	
 	//调度场和逆波兰函数定义
 	Stack<String> operators = new Stack<String>();	//存储操作符
 	Stack<String>  operands= new Stack<String>();	//存储操作数
-//	private static StringBuilder postfixExpression=new StringBuilder(); //展示表达式
+	StringBuilder postfixExpression=new StringBuilder(); //展示表达式
 	
 	String[] str_ArithmeticProblem = new String[10000];	//存放算术题
 	String[] str_MathAnswer = new String[10000];	//存放题目和标准答案
 	
+	public MathExam6335() {
+		// TODO Auto-generated constructor stub
+		
+	}
+	
 	public MathExam6335(String[] args){
-		inPut(args[0], args[1],args[2],args[3]);
+		inPut(args);
 		mathProblem(count);
 		outPut();
 	}
 
-	private void inPut(String str0,String str1, String str2, String str3) {
+	public void inPut(String[] str) {
 		// TODO Auto-generated method stub
 		boolean flag1 = true;		//判断题目年级参数
 		boolean flag2 = true;
@@ -71,43 +80,43 @@ public class MathExam6335 {
 		
 		while (true) {		
 			try {
-				if(str0.equals("-n") && str2.equals("-grade")){		//首先匹配输入的参数类型，args[0] 和 args[2] 的内容
-					matcher1 = pattern1.matcher(str1);		
-					matcher2 = pattern2.matcher(str3);
+				if(str[0].equals("-n") && str[2].equals("-grade")){		//首先匹配输入的参数类型，args[0] 和 args[2] 的内容
+					matcher1 = pattern1.matcher(str[1]);		
+					matcher2 = pattern2.matcher(str[3]);
 					flag1 = matcher1.matches();
 					flag2 = matcher2.matches();
 					if(!flag1 || !flag2){
 						throw new NumberFormatException();
 					} else {
-						count = Integer.valueOf(str1);
-						grade = Integer.valueOf(str3);
+						count = Integer.valueOf(str[1]);
+						grade = Integer.valueOf(str[3]);
 					}
-				}else if(str0.equals("-grade") && str2.equals("-n")){		//regex1和3的表达式都匹配成功
-					matcher1 = pattern1.matcher(str3);
-					matcher2 = pattern2.matcher(str1);
+				}else if(str[0].equals("-grade") && str[2].equals("-n")){		//regex1和3的表达式都匹配成功
+					matcher1 = pattern1.matcher(str[3]);
+					matcher2 = pattern2.matcher(str[1]); 
 					flag1 = matcher1.matches();		//题数匹配规范性
 					flag2 = matcher2.matches();		//年级匹配规范性
 					if(!flag1 || !flag2){		//两个参数不符合正则表达式规范就进入异常
 						throw new NumberFormatException();		
 					} else {	
-						count = Integer.valueOf(str3);
-						grade = Integer.valueOf(str1);
+						count = Integer.valueOf(str[3]);
+						grade = Integer.valueOf(str[1]);
 					}
 				} else {
 					System.out.println("输入参数类型有误，即将退出程序");
-					System.exit(0);
+					w1 = false;
 				}				
 			} catch (NumberFormatException e) {
 				// TODO: handle exception
 				System.out.println("输入参数不符合规范，即将退出程序");
-				System.exit(0);
+				w2 = false;
 			}			
 			in.close();
 			break;
 		}
 	}
 	
-	private void outPut() {
+	public void outPut() {
 		// TODO Auto-generated method stub
 		File file = new File("out.txt");
 		
@@ -116,7 +125,7 @@ public class MathExam6335 {
 				file.createNewFile();
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
-				System.out.println("文件创建时出现错误!");
+				System.out.println("文件创建时出现错误! 即将退出程序....");
 			}
 		}
 		
@@ -144,14 +153,14 @@ public class MathExam6335 {
 			System.out.print("-------  本次共生成" + count + "道小学"+ grade + "年级算数题，请打开out.txt文件查看详情    -------"); 	
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
-			System.out.println("未找到指定文件!");
+			System.out.println("未找到指定文件! 即将退出程序....");
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
-			System.out.println("文件写入有误!");
+			System.out.println("文件写入有误! 即将退出程序....");
 		} 	
 	}
 	
-    private static boolean isOperator(String operator){
+	public static boolean isOperator(String operator){
     	//判断操作符
         if (operator.equals("+")||operator.equals("-")||operator.equals("×")||operator.equals("÷")
                 ||operator.equals("(")||operator.equals(")")) {
@@ -160,7 +169,7 @@ public class MathExam6335 {
         return false;
     }
 	
-    private static int priority(String s){
+	public static int priority(String s){
     	//操作符优先级
         if (s.equals("+") || s.equals("-")) {
         	return 1;
@@ -169,11 +178,11 @@ public class MathExam6335 {
         } else if(s.equals("(") || s.equals(")")){
         	return 3;
         } else{
-        	return 0;
+        	return 0; 
         }
     }
     
-    private static int Calculation(int n,int m,String operator){
+	public static int Calculation(int n,int m,String operator){
     	//计算结果
     	int result = -4567;
     	if (operator.equals("+")) {
@@ -201,7 +210,7 @@ public class MathExam6335 {
     	return result;
     }
 
-	private void toPostfixExpression(String str_mix){
+	public String toPostfixExpression(String str_mix){ 
 		//逆波兰算法——[中缀表达式转后缀表达式]
 		int len = str_mix.length();	//获取表达式的长度
 		char ch;	//接受charAt()方法获取的的字符
@@ -237,15 +246,13 @@ public class MathExam6335 {
 						while(!operators.empty() && !operators.peek().equals("(")	//栈不为空，当前栈顶操作符不为左括号
 								&& priority(operators.peek()) >= priority(sc)){		//操作符优先级小于等于当前栈顶操作符优先级
 							do {
-//								operator_Add = operators.pop();
-//								postfixExpression.append(operator_Add);
+								postfixExpression.append(operators.peek());
 								operands.push(operators.pop());
 							} while (false);	}	// 栈顶操作符是左括号时停止压栈
 						operators.push(sc);		//否则直接入栈
 					} else if(sc.equals(")")){	//当前扫描到的操作符为右括号(不做入栈操作)，依次压栈相匹配的左括号内容
 						do {
-//							operator_Add = operators.pop();
-//							postfixExpression.append(operator_Add);
+							postfixExpression.append(operators.peek());
 							operands.push(operators.pop());
 						} while (!operators.peek().equals("("));
 						operators.pop();	//弹出栈顶无用操作符左括号
@@ -254,17 +261,21 @@ public class MathExam6335 {
 				
 			} else {	//非操作符
 				if(!sc.equals(" ")){
+					postfixExpression.append(sc);
 					operands.push(sc);	
 				}
 			}
 		}
 		
 		while(!operators.empty()){	//结束字符串扫描后操作符的栈不为空则则压栈
+			postfixExpression.append(operators.peek());
 			operands.push(operators.pop());
 		}
+//		System.out.println(String.valueOf(postfixExpression));
+		return String.valueOf(postfixExpression);
 	}
 	
-	private int reversePolish() {
+	public int reversePolish() {
 		// TODO Auto-generated method stub
 		//计算后缀表达式
 		String oper;	//存储栈里的元素
@@ -279,7 +290,7 @@ public class MathExam6335 {
 			if(!isOperator(oper)){	//判断非操作符，入数组栈栈
 				postfixNumber.push(Integer.parseInt(oper));
 			} else{
-				int m = postfixNumber.pop();	//返回符号两边的数字
+				int m = postfixNumber.pop();	//返回符号两边的数字 
 				int n = postfixNumber.pop();
 				if (Calculation(n, m, oper) < 0) {	//计算后返回值为负数，则表达式存在“差值为负”或者“除数为0”的情况，并跳出循环
 					calflag = false;
@@ -305,7 +316,7 @@ public class MathExam6335 {
 		}
 	}
 	
-	private void mathProblem(int count) {	
+	public void mathProblem(int count) {	
 		//生成算术题
 		Random rnd = new Random();
 		
@@ -346,7 +357,7 @@ public class MathExam6335 {
 		}
 	}
 	
-	private String operator() {		
+	public String operator() {		
 		// TODO Auto-generated method stub
 		//随机生成操作符
 		Random in =new Random();
@@ -354,12 +365,12 @@ public class MathExam6335 {
 		return ro[in.nextInt(4)];
 	}
 	
-	private int operand() {
+	public int operand() {
 		// TODO Auto-generated method stub
 		return (int)(Math.random()*100+1);
 	}
 	
-	private void fourMixed(int i) {
+	public void fourMixed(int i) {
 		// TODO Auto-generated method stub
 		//四则运算：括号算操作符，总共2-4个操作符
 		int res = 0; 
@@ -425,7 +436,7 @@ public class MathExam6335 {
 			
 	}
 	
-	private void add(int n1, int n2,int i) {
+	public int add(int n1, int n2,int i) {
 		// TODO Auto-generated method stub
 		/*
 		 * 加法：
@@ -435,9 +446,10 @@ public class MathExam6335 {
 		result = n1 + n2;
 		str_ArithmeticProblem[i] = "( " + (i+1) +" ) " + n1 + " + " + n2;
 		str_MathAnswer[i] = "( " + (i+1) +" ) " + n1 + " + " + n2 + " = " + result;
+		return result;
 	}
 	
-	private void sub(int n1, int n2,int i) {
+	public int sub(int n1, int n2,int i) {
 		// TODO Auto-generated method stub
 		/*
 		 * 减法：
@@ -454,9 +466,10 @@ public class MathExam6335 {
 		result = n1 - n2;
 		str_ArithmeticProblem[i] = "( " + (i+1) +" ) " + n1 + " - " + n2;
 		str_MathAnswer[i] = "( " + (i+1) +" ) " + n1 + " - " + n2 + " = " + result;
+		return result;
 	}
 	
-	private void mul(int n1, int n2,int i) {
+	public int mul(int n1, int n2,int i) {
 		// TODO Auto-generated method stub
 		/*
 		 * 乘法：
@@ -473,9 +486,10 @@ public class MathExam6335 {
 		result = n1 * n2;
 		str_ArithmeticProblem[i] = "( " + (i+1) +" ) " + n1 + " × " + n2;
 		str_MathAnswer[i] = "( " + (i+1) +" ) " + n1 + " × " + n2 + " = " + result;
+		return result;
 	}
 	
-	private void div(int i) {
+	public void div(int i) {
 		// TODO Auto-generated method stub
 		/*
 		 * 除法：
@@ -498,6 +512,33 @@ public class MathExam6335 {
 			}
 			break;
 		}
+	}
+	
+	public String div(int n1,int n2,int i) {
+		// TODO Auto-generated method stub
+		/*
+		 * 除法：
+		 * 
+		 * 1.一二年级的除法运算应该在”九九乘法表“范围以内；
+		 * 2.分母不能为”0“。
+		 * 
+		 */
+		String res = null;
+		while(true){
+			if(n1 % n2 == 0){
+				result = n1 / n2;
+				str_ArithmeticProblem[i] = "( " + (i+1) +" ) " + n1 + " ÷ " + n2;
+				str_MathAnswer[i] = "( " + (i+1) +" ) " + n1 + " ÷ " + n2 + " = " + result;
+				res = String.valueOf(result);
+			}else if(n1 % n2 != 0){
+				result = n1 / n2;
+				str_ArithmeticProblem[i] = "( " + (i+1) +" ) " + n1 + " ÷ " + n2;
+				str_MathAnswer[i] = "( " + (i+1) +" ) " + n1 + " ÷ " + n2 + " = " + result + "..." + (n1 % n2);
+				res = String.valueOf(result) + "..." + String.valueOf((n1 % n2));
+			} 
+			break;
+		}
+		return res;
 	}
 	
 	
