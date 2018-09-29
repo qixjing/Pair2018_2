bd0bce6f72a122687ca74cf31d1e3c5ceadc9b8b
tt9250872 <42865280+tt9250872@users.noreply.github.com>
2018-09-30 03:23:15 +0800

    测试代码更新
    增添了判断生成题目数量和传入参数的比较。

--- Pair2_211606343_211606360/test/MathExam6360Test.java
+++ Pair2_211606343_211606360/test/MathExam6360Test.java
@@ -1,5 +1,4 @@
-import static org.junit.Assert.*;
-
+import static org.junit.Assert.assertEquals;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,107 +6,113 @@ import org.junit.Test;
 
 public class MathExam6360Test {
 
-	@Test
+	@Test   //测试优先级的判断是否正确
 	public void testLevel_1() {
 		String operation = "+";
 		 assertEquals(1, MathExam6360.Level(operation));
 	}
-	@Test
+	@Test   //测试优先级的判断是否正确
 	public void testLevel_2() {
 		String operation = "-";
 		 assertEquals(1, MathExam6360.Level(operation));
 	}
-	@Test
+	@Test   //测试优先级的判断是否正确
 	public void testLevel_3() {
 		String operation = "×";
 		 assertEquals(2, MathExam6360.Level(operation));
 	}
-	@Test
+	@Test   //测试优先级的判断是否正确
 	public void testLevel_4() {
 		String operation = "÷";
 		 assertEquals(2, MathExam6360.Level(operation));
 	}
-	@Test
+	@Test   //测试优先级的判断是否正确
 	public void testLevel_5() {
 		String operation = "";
 		 assertEquals(0, MathExam6360.Level(operation));
 	}
 	
-	@Test
+	@Test   //测试传入的参数格式是否正确
 	public void testMathExam_1(){
 		String[] str = new String[] {"-n", "10", "-grade", "3"};
 		assertEquals(true,MathExam6360.MathExam(str));
 	}
-	@Test
+	@Test  //测试传入的参数格式是否正确
 	public void testMathExam_2(){
 		String[] str = new String[] {"-grade", "3", "-n", "10"};
 		assertEquals(true,MathExam6360.MathExam(str));
 	}
-	@Test
+	@Test  //测试传入的参数格式是否正确
 	public void testMathExam_3(){
 		String[] str = new String[] {"-n", "10000", "-grade", "3"};
 		assertEquals(false,MathExam6360.MathExam(str));
 	}
-	@Test
+	@Test  //测试传入的参数格式是否正确
 	public void testMathExam_4(){
 		String[] str= {"-grade","3.0","-n","10"};
 		assertEquals(false,MathExam6360.MathExam(str));
 	}
-	@Test
+	@Test  //测试传入的参数格式是否正确
 	public void testMathExam_5(){
 		String[] str= {"-grade","-3","-n","10"};
 		assertEquals(false,MathExam6360.MathExam(str));
 	}
-	@Test
+	@Test  //测试传入的参数格式是否正确
 	public void testMathExam_6(){
 		String[] str= {"-grade","a","-n","b"};
 		assertEquals(false,MathExam6360.MathExam(str));
 	}
 
-	@Test
+	@Test  //测试传入的题目数量和年级是否符合大小限定以及格式
 	public void testJudge_1() {
 		String count = "10";
 		String grade = "3";
 		assertEquals(true,MathExam6360.judge(count, grade));
 	}
-	@Test
+	
+	@Test   //测试传入的题目数量和年级是否符合大小限定以及格式
 	public void testJudge_2() {
 		String count = "-10";
-		String grade = "3";
+		String grade = "2";
 		assertEquals(false,MathExam6360.judge(count, grade));
 	}
-	@Test
+	
+	@Test   //测试传入的题目数量和年级是否符合大小限定以及格式
 	public void testJudge_3() {
 		String count = "10";
-		String grade = "3.1";
+		String grade = "5";
 		assertEquals(false,MathExam6360.judge(count, grade));
 	}
-	@Test
+	
+	@Test   //测试传入的题目数量和年级是否符合大小限定以及格式
 	public void testJudge_4() {
 		String count = "10000";
 		String grade = "3";
 		assertEquals(false,MathExam6360.judge(count, grade));
 	}
-	@Test
+	
+	@Test   //测试传入的题目数量和年级是否符合大小限定以及格式
 	public void testJudge_5() {
 		String count = "10";
 		String grade = "-3";
 		assertEquals(false,MathExam6360.judge(count, grade));
 	}
-	@Test
+	
+	@Test   //测试传入的题目数量和年级是否符合大小限定以及格式
 	public void testJudge_6() {
 		String count = "10.2";
 		String grade = "3";
 		assertEquals(false,MathExam6360.judge(count, grade));
 	}
-	@Test
+	
+	@Test   //测试传入的题目数量和年级是否符合大小限定以及格式
 	public void testJudge_7() {
 		String count = "ab";
 		String grade = "ac";
 		assertEquals(false,MathExam6360.judge(count, grade));
 	}
 
-	@Test
+	@Test  //测试逆波兰算法是否正确
 	public void testParseSuffixExpression() {
 		 List<String> test=new ArrayList<String>();
 	        test.add("1");
@@ -132,7 +137,7 @@ public class MathExam6360Test {
 	        assertEquals(answer, MathExam6360.parseSuffixExpression(test));
 	}
 
-	@Test
+	@Test  //测试中序表达式是否正确
 	public void testToInfixExpression() {
 		String test= "1+2-3×4÷5";
         List<String> answer=new ArrayList<String>();
@@ -148,7 +153,7 @@ public class MathExam6360Test {
         assertEquals(answer, MathExam6360.toInfixExpression(test));
 	}
 
-	@Test
+	@Test  //测试计算方式是否正确
 	public void testReckon() {
 		List<String> test=new ArrayList<String>();
 	    test.add("1");
@@ -163,6 +168,23 @@ public class MathExam6360Test {
 	    String answer="1";
 	    assertEquals(answer, MathExam6360.reckon(test));
 	}
-
+	
+	@Test  //测试生成题目数量是否正确
+	public void testCalculate_1() {
+		int count = 100;
+		assertEquals(count, MathExam6360.calculate_1(count));
+	}
+	
+	@Test  //测试生成题目数量是否正确
+	public void testCalculate_2() {
+		int count = 100;
+		assertEquals(count, MathExam6360.calculate_2(count));
+	}
+	
+	@Test  //测试生成题目数量是否正确
+	public void testCalculate_3() {
+		int count = 100;
+		assertEquals(count, MathExam6360.calculate_3(count));
+	}
 
 }
