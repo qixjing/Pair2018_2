d6c06fa62a64d83769b953300488060b52faa0ae
zklowens <575940907@qq.com>
2018-09-30 08:44:12 +0800

    测试套件的使用

---
+++ Pair2_211614269_211601233/test/AllTest.java
@@ -0,0 +1,8 @@
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({CalculateTest.class,MathExamTest.class})
+public class AllTest {
+}
