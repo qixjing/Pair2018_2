d0ae8f7869e0dc5048ac32f73c4ac447112a0183
LIUZIMING6 <632095613@qq.com>
2018-09-30 08:24:58 +0800

    单元测试
    第三次作业

---
+++ Pair_211601253_211601246/test/MathEaxmtest.java
@@ -0,0 +1,23 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MathEaxmtest {
+
+	@Test
+	public void test2() {
+		String[] abc= {"-n","10","-grade","2"};
+		MathExam1253.main(abc);
+	}
+	@Test
+	public void test3() {
+		String[] abc= {"-n","10","-grade","3"};
+		MathExam1253.main(abc);
+	}
+	@Test
+	public void test1() {
+		String[] abc= {"-n","10","-grade","1"};
+		MathExam1253.main(abc);
+	}
+
+}
